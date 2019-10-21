2d3ff4206e54c0c72ea14a8dbf80b75f16700d18
FunnyLee <13066807136@163.com>
2019-10-21 18:28:34 +0800

    commit module

---
+++ wan_android/src/main/java/com/funny/wan/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.funny.wan.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.funny.wan.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

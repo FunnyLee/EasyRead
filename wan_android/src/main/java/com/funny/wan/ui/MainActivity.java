cc79b36d1c9737e60fc2f53398cb19a954eba2aa
FunnyLee <13066807136@163.com>
2019-10-30 17:30:29 +0800

    commit

--- wan_android/src/main/java/com/funny/wan/ui/MainActivity.java
+++ wan_android/src/main/java/com/funny/wan/ui/MainActivity.java
@@ -1,8 +1,5 @@
 package com.funny.wan.ui;
 
-import android.content.Context;
-import android.content.Intent;
-
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.funny.component.base.BaseActivity;
 import com.funny.component.router.RouterManager;
@@ -10,12 +7,6 @@ import com.funny.wan.R;
 
 @Route(path = RouterManager.MAIN_ACTIVITY)
 public class MainActivity extends BaseActivity {
-
-    public static void start(Context context) {
-        Intent starter = new Intent(context, MainActivity.class);
-//        starter.putExtra();
-        context.startActivity(starter);
-    }
 
     @Override
     protected int getLayoutId() {

269718302011559bc29469b10d43b90b2b06ef81
FunnyLee <13066807136@163.com>
2019-11-04 11:38:01 +0800

    commit main

--- wan_android/src/main/java/com/funny/wan/ui/MainActivity.java
+++ wan_android/src/main/java/com/funny/wan/ui/MainActivity.java
@@ -4,12 +4,20 @@ import com.alibaba.android.arouter.facade.annotation.Route;
 import com.funny.component.base.BaseActivity;
 import com.funny.component.router.RouterManager;
 import com.funny.wan.R;
+import com.funny.wan.databinding.ActivityMainBinding;
+import com.funny.wan.viewModel.MainViewModel;
+import com.gyf.immersionbar.ImmersionBar;
 
 @Route(path = RouterManager.MAIN_ACTIVITY)
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity<MainViewModel, ActivityMainBinding> {
 
     @Override
     protected int getLayoutId() {
         return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initImmersionbar() {
+        ImmersionBar.with(this).statusBarView(mBingdingView.statusView).init();
     }
 }
---
+++ wan_android/src/main/java/com/funny/wan/viewModel/MainViewModel.java
@@ -0,0 +1,18 @@
+package com.funny.wan.viewModel;
+
+import android.app.Application;
+import android.support.annotation.NonNull;
+
+import com.funny.component.base.BaseViewModel;
+
+/**
+ * Author: Funny
+ * Time: 2019/11/4
+ * Description: This is MainViewModel
+ */
+public class MainViewModel extends BaseViewModel {
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
